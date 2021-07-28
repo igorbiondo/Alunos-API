870c5fd8ef8eab6b3bca211d3eb01705bd05d329
DESKTOP-F4071BP\igorb <igorbiondo@gmail.com>
2021-07-28 17:41:25 -0300

    first commit

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import userController from '../controllers/UserController';
+const router = new Router();
+
+router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
+
+export default router;
+
+/*
+index -> lista todos os usuarios, GET
+store/create -> cria um novo usuario, POST
+delete -> apaga um usuario DELETE
+show -> mostra um usuario GET
+update -> atualiza um usuario PATCH OU PUT
+*/
